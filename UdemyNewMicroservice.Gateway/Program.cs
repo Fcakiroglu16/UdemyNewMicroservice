b11fc11b2040680c5bffddc66e340eb0db33ed19
fcakiroglu16 <f-cakiroglu@outlook.com>
2025-07-30 00:05:36 +0300

    Enhance authentication and authorization setup
    Updated `Program.cs` to implement a new authentication and authorization extension, removing the authentication middleware in favor of authorization only. Added a project reference to `UdemyNewMicroservice.Shared` in the gateway project. Included an authorization header in the payment API POST request for bearer token authentication. Updated `appsettings.Development.json` to define authorization policies for reverse proxy routes and standardized route transforms. Removed the `IdentityOption` section, indicating a change in identity management strategy.

--- UdemyNewMicroservice.Gateway/Program.cs
+++ UdemyNewMicroservice.Gateway/Program.cs
@@ -1,8 +1,14 @@
+using UdemyNewMicroservice.Shared.Extensions;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddReverseProxy().LoadFromConfig(builder.Configuration.GetSection("ReverseProxy"));
+
+
+builder.Services.AddAuthenticationAndAuthorizationExt(builder.Configuration);
 var app = builder.Build();
 app.MapReverseProxy();
 app.MapGet("/", () => "YARP (Gateway)");
-
+app.UseAuthentication();
+app.UseAuthorization();
 app.Run();
