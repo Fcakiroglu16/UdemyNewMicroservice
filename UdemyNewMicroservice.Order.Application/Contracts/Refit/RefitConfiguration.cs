a06209813cfcea94ab68f72b8d6b2f0b189ef7bf
fcakiroglu16 <f-cakiroglu@outlook.com>
2025-09-08 16:59:17 +0300

    Add client secret management and authentication handler
    Updated appsettings.Development.json to include ClientSecretOption for secure client credentials. Enhanced RefitConfiguration to bind IdentityOption and ClientSecretOption as singletons. Introduced ClientAuthenticatedHttpClientHandler for handling HTTP requests with access tokens. Added ClientSecretOption class for configuration binding. Updated project file to include Duende.IdentityModel for identity management.

--- UdemyNewMicroservice.Order.Application/Contracts/Refit/RefitConfiguration.cs
+++ UdemyNewMicroservice.Order.Application/Contracts/Refit/RefitConfiguration.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
 using Refit;
 using UdemyNewMicroservice.Order.Application.Contracts.Refit.PaymentService;
 using UdemyNewMicroservice.Shared.Options;
@@ -12,6 +13,21 @@ namespace UdemyNewMicroservice.Order.Application.Contracts.Refit
             IConfiguration configuration)
         {
             services.AddScoped<AuthenticatedHttpClientHandler>();
+
+
+            services.AddOptions<IdentityOption>().BindConfiguration(nameof(IdentityOption)).ValidateDataAnnotations()
+                .ValidateOnStart();
+
+            services.AddSingleton<IdentityOption>(sp => sp.GetRequiredService<IOptions<IdentityOption>>().Value);
+
+
+            services.AddOptions<ClientSecretOption>().BindConfiguration(nameof(ClientSecretOption))
+                .ValidateDataAnnotations()
+                .ValidateOnStart();
+
+            services.AddSingleton<ClientSecretOption>(sp =>
+                sp.GetRequiredService<IOptions<ClientSecretOption>>().Value);
+
 
             services.AddRefitClient<IPaymentService>().ConfigureHttpClient(configure =>
             {
---
+++ UdemyNewMicroservice.Shared/Options/ClientSecretOption.cs
@@ -0,0 +1,8 @@
+﻿namespace UdemyNewMicroservice.Shared.Options
+{
+    public class ClientSecretOption
+    {
+        public required string Id { get; set; }
+        public required string Secret { get; set; }
+    }
+}
